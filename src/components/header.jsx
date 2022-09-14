a3aa5e2fcddca909f284bb4f53b37b47f46c011c
Otniel Silva <55518288+otnielgusta@users.noreply.github.com>
2022-09-14 09:33:00 -0300

    introducao

---
+++ src/components/header.jsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+
+export default function Header() {
+    const router = useRouter();
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.containerHeader}>
+                <div className={styles.botoes}>
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        router.push({
+                            pathname: '/'
+                        })
+                    }} >Início</button>
+
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        router.push({
+                            pathname: '/introducao'
+                        })
+                    }}>Introdução</button>
+
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        router.push({
+                            pathname: '/mobile'
+                        })
+                    }}>Mobile</button>
+
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        router.push({
+                            pathname: '/video'
+                        })
+                    }}>ideo</button>
+
+                </div>
+
+            </div>
+        </header>
+    )
+}
---
+++ src/pages/introducao/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+import Subtitulo from '../../components/subtitulo'
+
+
+export default function Introducao() {
+  return (
+    <div className={styles.container}>
+      <Subtitulo
+        titulo="Introdução"
+        subtitulo="O que é Android?"
+        itens={[
+          "Sistema Operacional Open Source.",
+          "Fundado em 2003 pela Android Inc.",
+          "Sistema Operacional Mobile mais utilizado do mundo."
+        ]}
+      />
+      <Spacer />
+      <Subtitulo
+        titulo="Introdução"
+        subtitulo=""
+        itens={[
+          "Em 2005 o Google comprou a Android Inc.",
+          "Em 2007 a criação da Open Handset Alliance"
+        ]}
+      />
+
+      <Spacer />
+      <Subtitulo
+        titulo="Introdução"
+        subtitulo=""
+        itens={[
+          "Lançamento do primeiro android comercial em 2008, o HTC DREAM"
+        ]}
+        imagem="img1.png"
+      />
+
+    </div>
+  )
+}
